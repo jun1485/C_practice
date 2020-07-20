f7c4de2de30d81df09af9c5189c6010b88f3fe1a
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-07-20 17:39:57 +0900

    Create 1차원 배열 1.c

---
+++ C_practice/1차원 배열 1.c
@@ -0,0 +1,20 @@
+#define _CRT_SUCURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int arr[100] = { 0 };
+	int n, k, res = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &k);
+		arr[i] = k;
+	}
+	res = (arr[0] < arr[1]) ? arr[0] : arr[1];
+	for (int i = 0; i < n; i++)
+	{
+		if (res > arr[i]) res = arr[i];
+	}
+	printf("%d", res);
+}
