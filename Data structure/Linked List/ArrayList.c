0db9ecd237764bc4849e9437f34a2d8dcc266e18
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-07-29 00:12:11 +0900

    Update ArrayList.c

--- Data structure/Linked List/ArrayList.c
+++ Data structure/Linked List/ArrayList.c
@@ -30,10 +30,31 @@ int LFirst(List* plist, LData* pdata)
 
 int LNext(List* plist, LData* pdata)
 {
-	if (plist->curPosition > plist->numOfData)
+	if (plist->curPosition >= plist->numOfData-1)
 		return FALSE;
 
 	plist->curPosition++;
 	*pdata = plist->arr[plist->curPosition];
 	return TRUE;
+}
+
+LData LRemove(List* plist)
+{
+	int rpos = plist->curPosition;
+	int num = plist->numOfData;
+	LData rdata = plist->arr[rpos];
+
+	for (int i = rpos; i < num-1; i++)
+	{
+		plist->arr[i] = plist->arr[i+1];
+	}
+
+	plist->numOfData--;
+	plist->curPosition--;
+	return rdata;
+}
+
+int LCount(List* plist)
+{
+	return plist->numOfData;
 }
