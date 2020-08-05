73bd3be230c5fa7229e78a41dab49ac6f2b08ce2
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-05 16:11:28 +0900

    Update DLinkedRead.c

--- Data structure/Linked List/DLinkedRead.c
+++ Data structure/Linked List/DLinkedRead.c
@@ -1,53 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct _node
 {
 	int data;
-	struct _node* next;
+	struct _node * next;
 } Node;
 
 int main(void)
 {
-	Node* head = NULL;
-	Node* tail = NULL;
-	Node* cur = NULL;
+	Node * head = NULL;
+	Node * tail = NULL;
+	Node * cur = NULL;
 
-	Node* newNode = NULL;
+	Node * newNode = NULL;
 	int readData;
 
-	head = (Node*)malloc(sizeof(Node));
-	tail = head;
+	head = (Node*)malloc(sizeof(Node));    // 추가 된 문장, 더미 노드 추가
+	tail = head;				// 더미 노드를 추가하는 이유 : 유효한 첫번째 데이터를 가지고 있는 노드와 유효한 두번째 데이터를 가지고 있는 노드의 달랐었는데
+						// 이것의 처리를 하나로 묶기 위해서
 
-	while (1)
+	/**** 데이터를 입력 받는 과정 ****/
+	while(1)
 	{
+		printf("자연수 입력: ");
 		scanf("%d", &readData);
-		if (readData < 1)
+		if(readData < 1)
 			break;
 
+		/*** 노드의 추가과정 ***/
 		newNode = (Node*)malloc(sizeof(Node));
 		newNode->data = readData;
 		newNode->next = NULL;
+
+		/*
+		if(head == NULL)
+			head = newNode;
+		else
+			tail->next = newNode;
+		*/
 	}
 	tail->next = newNode;
+
 	tail = newNode;
 
 	printf("\n");
 
 	/**** 입력 받은 데이터의 출력과정 ****/
 	printf("입력 받은 데이터의 전체출력! \n");
-	if (head == NULL)
+	if(head == NULL) 
 	{
-		printf("입력 받은 데이터가 없습니다! \n");
+		printf("저장된 자연수가 존재하지 않습니다. \n");
 	}
-	else
+	else 
 	{
 		cur = head;
 		cur = cur->next;
-		//	printf("%d  ", cur->data);   // 첫 번째 데이터 출력
-
-		while (cur != NULL)    // 두 번째 이후의 데이터 출력
+	//	printf("%d  ", cur->data);   // 첫 번째 데이터 출력
+		
+		while(cur != NULL)    // 두 번째 이후의 데이터 출력
 		{
 			cur = cur->next;
 			printf("%d  ", cur->data);
@@ -56,19 +67,19 @@ int main(void)
 	printf("\n\n");
 
 	/**** 메모리의 해제과정 ****/
-	if (head == NULL)
+	if(head == NULL) 
 	{
 		return 0;    // 해제할 노드가 존재하지 않는다.
 	}
-	else
+	else 
 	{
-		Node* delNode = head;
-		Node* delNextNode = head->next;
+		Node * delNode = head;
+		Node * delNextNode = head->next;
 
-		//	printf("%d을(를) 삭제합니다. \n", head->data);
-		//	free(delNode);    // 첫 번째 노드의 삭제
-
-		while (delNextNode != NULL)    // 두 번째 이후의 노드 삭제 위한 반복문
+	//	printf("%d을(를) 삭제합니다. \n", head->data);
+	//	free(delNode);    // 첫 번째 노드의 삭제
+		
+		while(delNextNode != NULL)    // 두 번째 이후의 노드 삭제 위한 반복문
 		{
 			delNode = delNextNode;
 			delNextNode = delNextNode->next;
