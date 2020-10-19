f53df3eb6ff8b044e95530c478992e496039b83a
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-10-19 19:39:44 +0900

    Update Linked_list.c

--- C_practice/Linked_list.c
+++ C_practice/Linked_list.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #define SWAP(a,b) {int t=a; a=b; b=t;}
 #define SIZE 20
 
@@ -8,3 +8,12 @@ typedef struct node {
     int data;
     struct node* next;
 }NODE;
+
+void randAlloc(int* arr) {
+    for (int i = 0; i < SIZE; i++)
+        arr[i] = rand() % 1000 + 1;
+}
+
+void selectionSort(int* arr) {
+    
+}
