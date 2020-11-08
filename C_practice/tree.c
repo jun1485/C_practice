7a51d176a9e92bd835a2d5b5fef952763c4ddfeb
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-11-08 14:45:25 +0900

    Create tree.c

---
+++ C_practice/tree.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node {
+	int data;
+	struct node* lchild;
+	struct node* rchild;
+};
+
+struct node* make_tree_by_code();
+void inorder(struct node*);
+void preorder(struct node*);
+void postorder(struct node*);
+struct node* copy(struct node*);
+struct node* swap(struct node*);
+int equal(struct node*, struct node*);
