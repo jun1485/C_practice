02d1e9fa51d4c491b989c1965ffbb90ab06f0145
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-11-11 16:33:36 +0900

    Update tree.c

--- C_practice/tree.c
+++ C_practice/tree.c
@@ -15,13 +15,24 @@ struct node* copy(struct node*);
 struct node* swap(struct node*);
 int equal(struct node*, struct node*);
 
+void main() {
+
+	struct node* A, * B, * C;
+
+	A = make_tree_by_code();
+	printf("Inorder_methode print >> "); inorder(A); printf("\n");
+	printf("Preorder_methode print >> "); preorder(A); printf("\n");
+	printf("Postorder_methode print >> "); postorder(A); printf("\n");
+	B = copy(A);
+	C = swap(A);
+}
+
 struct node* make_tree_by_code() {
 
 	int n = 0;
 	int data;
 	struct node* make_tree;
 	node ptr;
-	
 	make_tree = (struct node*)malloc(sizeof(struct node));
 	make_tree->rchild = make_tree->lchild = NULL;
 
@@ -29,3 +40,12 @@ struct node* make_tree_by_code() {
 	scanf_s("%d", &data);
 
 	int randData = rand() % 2;
+
+	make_tree->data = data;
+	if (data) {
+		make_tree->lchild = make_tree_by_code();
+		make_tree->rchild = make_tree_by_code();
+
+		return make_tree;
+	}
+	return NULL;
