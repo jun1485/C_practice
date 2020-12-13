efae36b38c3559b0eebcf06cf1016131b8967e87
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-12-13 21:57:26 +0900

    Create Sortings.c

---
+++ C_practice/Sortings.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 60000
+#define MAX_ELEMENT 60000
+#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
+
+typedef struct {
+	int key;
+} element;
+typedef struct {
+	element heap[MAX_ELEMENT];
+	int heap_size;
+} HeapType;
