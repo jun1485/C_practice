01d9b50bfba9230a784db10f88032baa284fa4d8
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-12-14 15:55:26 +0900

    Update Sortings.c

--- C_practice/Sortings.c
+++ C_practice/Sortings.c
@@ -14,3 +14,13 @@ typedef struct {
 	element heap[MAX_ELEMENT];
 	int heap_size;
 } HeapType;
+
+element a[MAX_SIZE];
+int sorted[MAX_SIZE];  
+double start, finish; 
+int selection_Sort[MAX_SIZE]; //선택 
+int insertion_Sort[MAX_SIZE]; //삽입 
+int merge_Sort[MAX_SIZE]; //합병 
+int heap_Sort[MAX_SIZE]; //히프 
+int n;
+
