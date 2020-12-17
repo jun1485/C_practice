089f28646a40cbed3d0c1bc63cc98b8435ecf233
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-12-17 18:29:03 +0900

    Update Sortings.c

--- C_practice/Sortings.c
+++ C_practice/Sortings.c
@@ -48,3 +48,16 @@ void selection_sort(int list[], int n)
 	}
 	finish = clock();  // 시간 끝             
 }
+
+		//삽입 정렬
+void insertion_sort(int list[], int n)
+{
+
+	int i, j, key;
+	for (i = 1; i < n; i++) {
+		key = list[i];
+		for (j = i - 1; j >= 0 && list[j] > key; j--)
+			list[j + 1] = list[j]; /* 레코드의 오른쪽 이동 */
+		list[j + 1] = key;
+	}
+}
