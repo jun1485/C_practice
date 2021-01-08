3f7c3fe0867364f85a31d8dab7c7721a9cf6d880
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-08 15:58:19 +0900

    Create Quicksort.c

---
+++ C_practice/Quicksort.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 60000
+#define MAX_ELEMENT 60000
+#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
