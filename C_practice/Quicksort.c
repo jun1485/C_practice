3015b38e50c5aa01f0606025e949e419d3890e57
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-08 15:59:25 +0900

    Update Quicksort.c

--- C_practice/Quicksort.c
+++ C_practice/Quicksort.c
@@ -6,3 +6,11 @@
 #define MAX_SIZE 60000
 #define MAX_ELEMENT 60000
 #define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
+
+typedef struct {
+	int key;
+} element;
+typedef struct {
+	element heap[MAX_ELEMENT];
+	int heap_size;
+} HeapType; 
