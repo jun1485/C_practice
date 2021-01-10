32f43d6b0455aa961349d58ff8fb4e4f44b2233a
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-10 22:26:09 +0900

    Update Quicksort.c

--- C_practice/Quicksort.c
+++ C_practice/Quicksort.c
@@ -19,3 +19,10 @@ element a[MAX_SIZE];
 int sorted[MAX_SIZE];   // 합병 정렬에서 추가 공간이 필요
 double start, finish; //시간을 재기위한 변수
 int quick_Sort[MAX_SIZE];
+
+int partition(int list[], int left, int right);                //퀵 정렬 
+void quick_sort(int list[], int left, int right);              //퀵 정렬 
+int n;
+
+void sort_clock();                                          
+ 
