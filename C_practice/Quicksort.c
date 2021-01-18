586e69ca5544e6c1e43a1b0c79995626b088bcc7
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-18 22:28:38 +0900

    Update Quicksort.c

--- C_practice/Quicksort.c
+++ C_practice/Quicksort.c
@@ -80,4 +80,8 @@ int partition(int list[], int left, int right)
 void quick_sort(int list[], int left, int right)
 {
 	if (left < right) {
-		int q = partition(list,
+		int q = partition(list, left, right);
+		quick_sort(list, left, q - 1);
+		quick_sort(list, q + 1, right);
+	}
+}
